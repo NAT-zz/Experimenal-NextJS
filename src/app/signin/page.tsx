c90bcc503c80f6e662fd29b835aed9326e4033b0
Nguyễn Anh Tuấn <olivertuan1310@gmail.com>
2025-03-18 16:35:01 +0700

    fix

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,7 +12,7 @@ function Page() {
         event.preventDefault();
 
         const { result, error } = await signIn(email, password);
-        // asdasdasdd
+
         if (error) {
             return console.log(error);
         }
